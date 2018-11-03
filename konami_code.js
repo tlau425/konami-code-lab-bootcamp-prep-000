47cc12a9e658c64b35ebad3ce87b0ee4d71a1eab
Tommy Lau <tlau425@yahoo.com>
2018-11-03 18:59:59 -0400

    initial commit

--- konami_code.js
+++ konami_code.js
@@ -13,4 +13,20 @@ const codes = [
 
 function init() {
   // your code here
-}
+  let i=0;
+  document.addEventListener('keydown', (e) => {
+    let key = e.key;
+    var requiredCode = codes[i];
+    if (key == requiredCode) {
+        i++;
+    } else {
+      i = 0;
+    }
+
+    if (i == codes.length) {
+      alert('hurray');
+      console.log("IT WORKED")
+      i = 0;
+    }
+  }
+)}
